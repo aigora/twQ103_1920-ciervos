0140c3d332f1c292634549fd6cd4dbfe5f93a3c3
FernandoCasillas <47816021+FernandoCasillas@users.noreply.github.com>
2020-04-01 15:09:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,62 +1,59 @@
-//
-//  main.c
-//  menu
-//
-//  Created by Jaime casas on 27/3/20.
-//  Copyright © 2020 Jaime casas. All rights reserved.
-//
 
+#include <math.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    int opc1,opc2;
-    char id, contraseña, nid, ncontraseña;
-    printf("1-Crear nuevo usuario:\n" );
-    printf("2-Iniciar sesion:\n");
-    printf("3-Salir.\n");
-    scanf("%d", &opc1);
+int main()																											
+{
     
-    
-    
-    switch (opc1)
-    {
-        case 1: printf( "Registar nuevo usuario: ");
-            scanf( "%s", &id );
-            printf( "Registar nueva contrasenia: ");
-            scanf( "%s", &contraseña);
-            break;
-            
-       
-        case 2:
-            printf( "Nombre usuario: ");
-            scanf( "%s", &nid );
-            printf( "Contraseña: ");
-            scanf( "%s", &ncontraseña );
-            printf("Qué desea?\n");
-            printf("1-Vender\n");
-            printf("2-Comprar\n");
-            printf("3-Alquilar\n");
-            scanf("%d",&opc2);
-            switch (opc2) {
-                case 1:
-                    printf("Para vender blablal\n");
-                    break;
-                case 2:
-                    printf("Para comprar blablabla\n");
-                    break;
-                case 3:
-                    printf("Para alquilar blabl\n");
-                default:
-                    printf("No has escogido una opcion valida\n");
-                    break;
-            }
-            break;
-            
-        case 3:
-            printf ("Saliendo...\n");
-    }
-    
-    
-    return 0;
+    	int opcion, opcion2;
+		//int vender,comprar,alquilar;
+		char id, contrasenia, nid, ncontrasenia;
+        printf("1-Crear nuevo usuario:\n" );
+        printf("2-Iniciar sesion:\n");
+		printf("3-Salir.\n");
+        scanf("%d", &opcion);
 
+        
+
+        switch (opcion)
+        {
+            case 1: printf( "Registar nuevo usuario: ");
+                    scanf( "%s", &id );
+                    printf( "Registar nueva contrasenia: ");
+                    scanf( "%s", &contrasenia);
+                    break;
+
+            case 2: printf( "Nombre usuario: ");
+                    scanf( "%s", &nid ); // Nombre de usuario guardado anteriormente
+                    printf( "Contrasenia:  ");
+                    scanf( "%s", &ncontrasenia ); //Contrasenia guardada anteriormente
+                    
+                   
+                    	printf("Elije un numero: 1-comprar, 2-vender o 3-alquilar\n");
+                    	scanf("%d",&opcion2);
+                    	
+                    	 switch (opcion2)
+                    	 {
+						case 1:
+							printf("Usted a elegido la opcion de comprar");
+							//scanf("%s", &comprar);
+							break;
+						case 2:
+							printf("Usted a elegido la opcion de vender");
+                    		//scanf("%s", &vender);
+                    		break;
+                    	case 3:
+                    		printf("Usted a elegido la opcion de alquilar");
+                    		//scanf("%s",&alquilar);
+                    	break;
+                	}
+                    break;
+                    
+            case 3:
+ 					printf ("Saliendo...\n");
+		}
+			
+			
+return 0;
 }
+
