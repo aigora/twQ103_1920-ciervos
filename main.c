ce1e314d693c89a611f9717411b0db3a0a4626c6
FernandoCasillas <47816021+FernandoCasillas@users.noreply.github.com>
2020-04-20 10:52:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ int main()
 {
     
     	int opcion, opcion2;
-	//char vender,comprar,alquilar;
 	char id, contrasenia, nid, ncontrasenia;
         printf("1-Crear nuevo usuario:\n" );
         printf("2-Iniciar sesion:\n");
@@ -29,21 +28,21 @@ int main()
                     scanf( "%s", &ncontrasenia ); //Contrasenia guardada anteriormente
                     
                    
-                    	printf("Elije un numero: 1-comprar, 2-vender o 3-alquilar\n");
+                    	printf("Elije un numero: 1-dar de alta, 2-dar de baja o 3-mostrar coches\n");
                     	scanf("%d",&opcion2);
                     	
                     	 switch (opcion2)
                     	 {
 						case 1:
-							printf("Usted a elegido la opcion de comprar");
+							printf("Usted a elegido la opcion de dar de alta");
 							//scanf("%s", &comprar);
 							break;
 						case 2:
-							printf("Usted a elegido la opcion de vender");
+							printf("Usted a elegido la opcion de dar de baja");
                     					//scanf("%s", &vender);
                     					break;
                     				case 3:
-                    					printf("Usted a elegido la opcion de alquilar");
+                    					printf("Usted a elegido la opcion de mostrar coches");
                     					//scanf("%s",&alquilar);
                     					break;
                 	}
